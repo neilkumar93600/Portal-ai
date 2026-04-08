211723d5d84eb5e310d32327f2a0e58f32682d0d
Neil <neil@example.com>
2026-04-08 21:37:47 +0530

    fix(calendar): update react-day-picker v9 components prop structure for chevron icons

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -60,12 +60,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
+        Chevron: ({ className, orientation, ...props }) => {
+          const Comp = orientation === "left" ? ChevronLeft : ChevronRight
+          return <Comp className={cn("size-4", className)} {...props} />
+        },
       }}
       {...props}
     />
